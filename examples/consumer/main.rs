f63e34e054fab1254094ded7a2ad3b41b974c206
lhmzhou <Linda.Zhou@aexp.com>
2020-01-04 11:04:37 -0500

    test-drive consumer API change

--- examples/consumer/main.rs
+++ examples/consumer/main.rs
@@ -1,5 +1,9 @@
 extern crate kafka;
 
+use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage};
+
+use failure::Error;
+
 fn main(){
     let broker = "localhost:8088".to_owned();
     let topic = "my-topic".to_owned();
@@ -8,4 +12,9 @@ fn main(){
     if let Err(e) = consume_msgs(group, topic, vec![broker]) {
         println!("Failed consuming messages: {}", e);
     }
+}
+
+
+async fn consume_messages(group: String, topic: String, brokers: Vec<String>) -> Result<(), Error> {
+    // add code here to customize the consumer behavior 
 }
